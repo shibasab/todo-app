56be558c7587203c57a30eedfd11320820b7d135
shibasab <34082145+shibasab@users.noreply.github.com>
2019-07-12 18:43:51 +0900

    todo app first commit

---
+++ todo/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+# todoリストに追加するもの
+class Post(models.Model):
+    title = models.CharField(max_length=200) #やること
+    detail = models.TextField() #やることの詳細
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def create(self):
+        self.created_date = timezone.now()
+        self.save()
+    def __str__(self):
+        return self.title
