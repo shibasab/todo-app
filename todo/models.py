5e859a9bc72039502e2def3d73ed88ff5cb6f8df
shibasab <34082145+shibasab@users.noreply.github.com>
2019-07-14 17:19:44 +0900

    ユーザーごとのtodoリストを表示できるように修正

--- todo/models.py
+++ todo/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # todoリストに追加するもの
 class Post(models.Model):
     title = models.CharField(max_length=200) #やること
     detail = models.TextField() #やることの詳細
     created_date = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def create(self):
         self.created_date = timezone.now()
--- todo/views.py
+++ todo/views.py
@@ -4,19 +4,23 @@ from django.utils import timezone
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    posts = Post.objects.filter(created_date__lte=timezone.now(), user=request.user).order_by('created_date')
     return render(request, 'todo/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'todo/post_detail.html', {'post': post})
-
+    if post.user == request.user:
+        return render(request, 'todo/post_detail.html', {'post': post})
+    else:
+        # ここにエラーメッセージを出したい
+        return redirect('post_list')
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.created_date = timezone.now()
+            post.user = request.user
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -25,13 +29,18 @@ def post_new(request):
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.created_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+    if post.user == request.user:
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.user = request.user
+                post.created_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'todo/post_edit.html', {'form': form})
     else:
-        form = PostForm(instance=post)
-    return render(request, 'todo/post_edit.html', {'form': form})
+        # ここにエラーメッセージを出したい
+        return redirect('post_list')
